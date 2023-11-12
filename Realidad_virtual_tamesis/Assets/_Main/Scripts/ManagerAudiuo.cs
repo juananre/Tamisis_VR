b6964b09be8c5354ed8996193658167e9b1853bd
juananre <78058130+juananre@users.noreply.github.com>
2023-11-12 01:52:49 -0500

    mucho

--- Realidad_virtual_tamesis/Assets/_Main/Scripts/ManagerAudiuo.cs
+++ Realidad_virtual_tamesis/Assets/_Main/Scripts/ManagerAudiuo.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerAudiuo : MonoBehaviour
+public class ManagerAudio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] AudioSource audioReproducir;
+    [SerializeField] AudioSource Inicio;
+    [SerializeField] AudioClip tico;
+    [SerializeField] AudioClip citri;
+    [SerializeField] AudioClip Alis;
+    [SerializeField] AudioClip nia;
+    [SerializeField] AudioClip tina;
+    bool pausa = false;
+    bool yaReproducido = false; // Nueva variable
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!yaReproducido && other.CompareTag("Player")) // Agregar la condición para verificar si ya se reprodujo
+        {
+            print("Sí funciona");
+
+            // Reproducir el clip de audio
+            if (audioReproducir != null)
+            {
+                if (CompareTag("tico") && tico != null)
+                {
+                    audioReproducir.clip = tico;
+                }
+                else if (CompareTag("citri") && citri != null)
+                {
+                    audioReproducir.clip = citri;
+                }
+                else if (CompareTag("alis") && Alis != null)
+                {
+                    audioReproducir.clip = Alis;
+                }
+                else if (CompareTag("nia") && nia != null)
+                {
+                    audioReproducir.clip = nia;
+                }
+                else if (CompareTag("tina") && tina != null)
+                {
+                    audioReproducir.clip = tina;
+                }
+                else if (CompareTag("pause"))
+                {
+                    audioReproducir.Stop();
+                }
+
+                audioReproducir.Play();
+                Inicio.Stop();
+                pausa = true;
+                yaReproducido = true; // Marcar como reproducido
+            }
+        }
     }
 }
